c37a714eb488554b060ff55277dedcf772549aae
zhaoxnshow <zhaoxnshow@163.com>
2022-04-20 09:24:46 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-button type="primary" size="small" @click="open">测试</el-button>
+
+  <router-view></router-view>
+</template>
+<script setup lang="ts">
+import { ElMessage } from 'element-plus'
+const open = () => {
+  ElMessage({ message: '测试' })
+}
+</script>
+
+<style></style>
